1220ad3b063d82e5bd06ab5c14c65bde2bafa275
Beatriz Eliza <beatrizelizarf@gmail.com>
2021-05-03 12:47:20 -0300

    Implementado H2 DataBase

--- src/main/java/com/devsuperior/myfirstproject/entities/Category.java
+++ src/main/java/com/devsuperior/myfirstproject/entities/Category.java
@@ -4,15 +4,28 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+@Entity
 public class Category implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String name;
 	
 	@JsonIgnore //para nao serializar 
+	@OneToMany(mappedBy = "category")
 	private List<Product> products = new ArrayList<>();
 	
 	public Category() {
